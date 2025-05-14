4b8a2d1da354bd195de06407761d9ac0745df287
PeuAlmeidaDev <peualmeida.dev@gmail.com>
2025-05-13 23:21:16 -0300

    Aprendendo react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
+import { useState } from 'react';
+import Modal from 'react-modal';
 
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-export function Header() {
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
   return (
     <Container>
         <Content>
             <img src={logo} alt="PBMoney" />
-            <button type="button">
+            <button type="button" onClick={onOpenNewTransactionModal}>
                 Nova transação
             </button>
         </Content>
+
     </Container>
   );
 }
